2194dfcf750f3a07abe6a49ec7f92b2dc80de660
zinglax <dylanzingler@gmail.com>
2014-12-15 23:28:20 -0500

    added a picture for the endoscope and a program for it that will take a picture

---
+++ endoscope.py
@@ -0,0 +1,32 @@
+import subprocess
+
+class Endoscope:
+    ''' Class for Endoscope webcam'''
+    resolution = "640x480 "
+        
+    def __init__(self, resolution):
+        self.resolution = resolution
+    
+    def takepic(self, file_and_path, banner=True):
+        ''' Takes a picture given a specified name, command should look like
+        fswebcam -r 640x480 --no-banner image3.jpg        
+        or something similar '''        
+        command = "fswebcam -r " + self.resolution
+        
+        # check to add banner
+        if banner == False:
+            command += "--no-banner "
+        
+        # adds the file path
+        command += file_and_path
+        
+        return_val = subprocess.call(command, shell=True)
+        
+        if return_val == 0:
+            print "picture taken!!!"
+        else:
+            print "oops the picture was not taken :-("
+        
+        
+    
+    
