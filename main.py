524d95fc869593c4b35fe246b4b6a26864a64ce3
Dylan Zingler <dylanzingler@gmail.com>
2015-01-03 15:05:52 -0500

    Update main.py
    Removed endoscope functionality. Working on garage pi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import led
 import ultrasonic
 import time
 
-e = endoscope.Endoscope()
+#e = endoscope.Endoscope()
 l = led.LED(17, "BCM")
 u = ultrasonic.UltraSonicSensor(24, 25, "BCM")
 
@@ -13,6 +13,6 @@ while True:
     print round(distance, 2)
     if 20 < distance < 30:
         l.blink(3)
-        e.takepic("test.jpg", False)
+        #e.takepic("test.jpg", False)
         
     time.sleep(.5)
