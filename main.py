483ba8257a57521dc7dd83166e68dbbfe75998aa
Dylan <dylanzingler@gmail.com>
2015-02-06 12:56:23 -0500

    added sensor alarm system for testing

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import endoscope
 import led
 import ultrasonic
 import time
+import webcam
 
 #e = endoscope.Endoscope()
-l = led.LED(17, "BCM")
-u = ultrasonic.UltraSonicSensor(24, 25, "BCM")
+#l = led.LED(17, "BCM")
+#u = ultrasonic.UltraSonicSensor(24, 25, "BCM")
 
 
-while True:
-    distance = u.read()
-    print round(distance, 2)
-    if 20 < distance < 30:
-        l.blink(3)
-        #e.takepic("test.jpg", False)
+#while True:
+    #distance = u.read()
+    #print round(distance, 2)
+    #if 20 < distance < 30:
+        #l.blink(3)
+        ##e.takepic("test.jpg", False)
         
-    time.sleep(.5)
+    #time.sleep(.5)
+
+c1 = webcam.Webcam(device='/dev/video0')
+c2 = webcam.Webcam(device='/dev/video1')
+c3 = webcam.Webcam(device='/dev/video2')
+
+cameras = [c1,c2,c3]
+
+for i, camera in enumerate(cameras):
+    camera.takepic('/webcam_pics/camera_'+str(i)+'_image.jpg')
+    
---
+++ sensor_alarm_system.py
@@ -0,0 +1,69 @@
+'''
+***RASPBERRY PI PROJECT***
+Sensor Alarm Systems (SAS)
+
+Author: Dylan Zingler
+Purpose: To notify recipiant of sensor detecting opened or closed door via Twitter posts
+Components: RGB-LED, Ultrasonic Sensor or PIR Sensor
+-
+'''
+
+from twython import Twython as tw
+from twython import TwythonStreamer
+from time import gmtime, strftime
+
+# Local libraries
+import endoscope
+
+class SensorAlarmSystem(TwythonStreamer):
+    consumer_key = "XJGitZ02u0wvfJ6L3WMqT1bRL"
+    consumer_secret = "Oxmq1v4DqfxW8JCcoLjSnuk8vGLwr7GJAwoNA5YrSszrPtARUI"
+    access_token = "403913218-y95Au89xBX6FDkjkBg95lHOPPkeJN8L0JyuJfnip"
+    access_token_secret = "5siPX5Rxe3AMGhVsl9iHA6QgJpHwxeGqIRzwugRExljCx"    
+    
+    on_off = False
+
+
+    def take_picture_send_tweet(self, data, twythonObj):
+        e = endoscope.Endoscope()        
+        
+        # Image name is ./twitter_images/TWITTER_USERNAME + DATE&TIME + .jpg
+        image_name = "./twitter_images/" + data['user']['screen_name'] + strftime("%Y-%m-%d_%H:%M:%S", gmtime()) + ".jpg"
+        e.takepic( image_name, False)            
+
+        photo = open(image_name, 'rb')
+        twythonObj.update_status_with_media(status='Checkout this cool image!  @' + data['user']['screen_name'], media=photo)        
+        
+    def on_success(self, data):
+        
+        if 'text' in data:
+            print data['user']['screen_name']
+            print data['text'].encode('utf-8') 
+
+            # Twython Interaction
+            t = tw(consumer_key, consumer_secret, 
+                  access_token, 
+                  access_token_secret)
+            
+            # Do something With sensors, send a tweet back maybe
+            #    Honestly its up to you. Play it cool.
+            
+            # Is your USB camera Setup?
+            # self.take_picture_send_tweet(data, t)
+            
+            print "WOW WHAT A GREAT DAY"
+
+if __name__=="__main__":
+    # OhChristmasTree (twitter app) credientials Under username TimsXmasTree
+    consumer_key = "XJGitZ02u0wvfJ6L3WMqT1bRL"
+    consumer_secret = "Oxmq1v4DqfxW8JCcoLjSnuk8vGLwr7GJAwoNA5YrSszrPtARUI"
+    access_token = "403913218-y95Au89xBX6FDkjkBg95lHOPPkeJN8L0JyuJfnip"
+    access_token_secret = "5siPX5Rxe3AMGhVsl9iHA6QgJpHwxeGqIRzwugRExljCx"  
+    
+    # Streaming
+    stream = Pic_Taker_Twitter(consumer_key,
+                               consumer_secret,
+                               access_token,
+                               access_token_secret)
+    stream.statuses.filter(track="#OONN")    
+    stream.statuses.filter(track="#OOFFFF")    
